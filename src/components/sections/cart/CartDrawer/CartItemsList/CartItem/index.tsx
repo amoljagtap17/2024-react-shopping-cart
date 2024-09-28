a2dce619c515d3fce6e2d0e813d02865721cccf7
amoljagtap17 <amoljagtap17@gmail.com>
2024-09-28 21:29:15 +0530

    react-shopping-cart: memoized cart item

---
+++ src/components/sections/cart/CartDrawer/CartItemsList/CartItem/index.tsx
@@ -0,0 +1,82 @@
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import RemoveIcon from "@mui/icons-material/Remove";
+import {
+  Box,
+  IconButton,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { memo } from "react";
+import { useCartDispatch } from "../../../../../../app/store";
+import { ICartItem } from "../../../../../../app/types";
+import { RenderCount } from "../../../../../lib";
+
+interface ICartItemProps {
+  item: ICartItem;
+}
+
+function CartItemUnmemoized({ item }: ICartItemProps) {
+  const dispatch = useCartDispatch();
+  const { id, price, quantity, name } = item;
+
+  return (
+    <ListItem
+      key={id}
+      secondaryAction={
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          color="error"
+          onClick={() => dispatch({ type: "REMOVE_ITEM", payload: id })}
+        >
+          <DeleteIcon />
+        </IconButton>
+      }
+      divider
+    >
+      <ListItemText
+        primary={name}
+        secondary={
+          <RenderCount bgcolor="info">${Number(price).toFixed(2)}</RenderCount>
+        }
+      />
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        width={140}
+      >
+        <IconButton
+          edge="end"
+          onClick={() => dispatch({ type: "DECREASE_QUANTITY", payload: id })}
+          disabled={quantity <= 1}
+          sx={{ display: "block", marginRight: 1 }}
+        >
+          <RemoveIcon />
+        </IconButton>
+        <Typography
+          variant="body1"
+          width={24}
+          height={30}
+          component="div"
+          padding={1}
+          boxSizing="content-box"
+          textAlign="center"
+        >
+          {quantity}
+        </Typography>
+        <IconButton
+          edge="end"
+          onClick={() => dispatch({ type: "INCREASE_QUANTITY", payload: id })}
+          sx={{ display: "block" }}
+        >
+          <AddIcon />
+        </IconButton>
+      </Box>
+    </ListItem>
+  );
+}
+
+export const CartItem = memo(CartItemUnmemoized);
--- src/components/sections/cart/CartDrawer/CartItemsList/index.tsx
+++ src/components/sections/cart/CartDrawer/CartItemsList/index.tsx
@@ -1,74 +1,10 @@
-import AddIcon from "@mui/icons-material/Add";
-import DeleteIcon from "@mui/icons-material/Delete";
-import RemoveIcon from "@mui/icons-material/Remove";
-import {
-  Box,
-  IconButton,
-  ListItem,
-  ListItemText,
-  Typography,
-} from "@mui/material";
-import { useCartDispatch, useCartItems } from "../../../../../app/store";
-import { RenderCount } from "../../../../lib";
+import { useCartItems } from "../../../../../app/store";
+import { CartItem } from "./CartItem";
 
 export function CartItemsList() {
   const cartItems = useCartItems();
-  const dispatch = useCartDispatch();
 
-  return cartItems.map(({ id, name, price, quantity }) => (
-    <ListItem
-      key={id}
-      secondaryAction={
-        <IconButton
-          edge="end"
-          aria-label="delete"
-          color="error"
-          onClick={() => dispatch({ type: "REMOVE_ITEM", payload: id })}
-        >
-          <DeleteIcon />
-        </IconButton>
-      }
-      divider
-    >
-      <ListItemText
-        primary={name}
-        secondary={
-          <RenderCount bgcolor="info">${Number(price).toFixed(2)}</RenderCount>
-        }
-      />
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        width={140}
-      >
-        <IconButton
-          edge="end"
-          onClick={() => dispatch({ type: "DECREASE_QUANTITY", payload: id })}
-          disabled={quantity <= 1}
-          sx={{ display: "block", marginRight: 1 }}
-        >
-          <RemoveIcon />
-        </IconButton>
-        <Typography
-          variant="body1"
-          width={24}
-          height={30}
-          component="div"
-          padding={1}
-          boxSizing="content-box"
-          textAlign="center"
-        >
-          {quantity}
-        </Typography>
-        <IconButton
-          edge="end"
-          onClick={() => dispatch({ type: "INCREASE_QUANTITY", payload: id })}
-          sx={{ display: "block" }}
-        >
-          <AddIcon />
-        </IconButton>
-      </Box>
-    </ListItem>
+  return cartItems.map((cartItem) => (
+    <CartItem item={cartItem} key={cartItem.id} />
   ));
 }
