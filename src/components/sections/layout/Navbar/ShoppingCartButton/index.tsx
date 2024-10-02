44e2fe6c43736937daff6a7db23ee1b458f149ea
amoljagtap17 <amoljagtap17@gmail.com>
2024-10-02 16:10:21 +0530

    start: shopping cart icon

--- src/components/sections/layout/Navbar/ShoppingCartButton/index.tsx
+++ src/components/sections/layout/Navbar/ShoppingCartButton/index.tsx
@@ -1,33 +1,33 @@
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
-import { Box, IconButton } from "@mui/material";
-import {
-  DrawerContents,
-  DrawerContextProvider,
-  DrawerTrigger,
-  RenderCount,
-} from "../../../../lib";
+import { Box, Drawer, IconButton } from "@mui/material";
+import { useState } from "react";
+import { RenderCount } from "../../../../lib";
 import { CartDrawer } from "../../../cart";
 
 export function ShoppingCartButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => setIsOpen(true);
+
   return (
     <Box>
-      <DrawerContextProvider>
-        <RenderCount bgcolor="success">
-          <DrawerTrigger>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="shopping cart"
-            >
-              <ShoppingCartCheckoutIcon />
-            </IconButton>
-          </DrawerTrigger>
-        </RenderCount>
-        <DrawerContents>
+      <RenderCount bgcolor="success">
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="shopping cart"
+          onClick={handleOpen}
+        >
+          <ShoppingCartCheckoutIcon />
+        </IconButton>
+      </RenderCount>
+
+      {isOpen ? (
+        <Drawer anchor="left" open={isOpen} onClose={() => setIsOpen(false)}>
           <CartDrawer />
-        </DrawerContents>
-      </DrawerContextProvider>
+        </Drawer>
+      ) : null}
     </Box>
   );
 }
