2515eb5bbfa6404f024b2df341e10ed49cb03f62
amoljagtap17 <amoljagtap17@gmail.com>
2024-10-03 23:35:54 +0530

    react-shopping-cart: refactor with reducers files

--- src/app/store/cart-context.tsx
+++ src/app/store/cart-context.tsx
@@ -1,58 +1,8 @@
-import { produce } from "immer";
 import { createContext, ReactNode, useContext, useReducer } from "react";
-import {
-  CART_ACTION_TYPES,
-  ICartAction,
-  ICartContextType,
-  ICartItem,
-} from "../types";
+import { CART_ACTION_TYPES, ICartContextType, ICartItem } from "../types";
+import { cartReducer } from "./reducers";
 
 const CartContext = createContext<ICartContextType | undefined>(undefined);
-
-const cartReducer = produce((draft: ICartItem[], action: ICartAction) => {
-  switch (action.type) {
-    case CART_ACTION_TYPES.ADD_ITEM: {
-      const existingItem = draft.find((item) => item.id === action.payload.id);
-
-      if (existingItem) {
-        existingItem.quantity += 1;
-      } else {
-        draft.push({ ...action.payload, quantity: 1 });
-      }
-
-      break;
-    }
-    case CART_ACTION_TYPES.REMOVE_ITEM: {
-      const index = draft.findIndex((item) => item.id === action.payload);
-
-      if (index !== -1) {
-        draft.splice(index, 1);
-      }
-
-      break;
-    }
-    case CART_ACTION_TYPES.INCREASE_QUANTITY: {
-      const item = draft.find((item) => item.id === action.payload);
-
-      if (item) {
-        item.quantity += 1;
-      }
-
-      break;
-    }
-    case CART_ACTION_TYPES.DECREASE_QUANTITY: {
-      const item = draft.find((item) => item.id === action.payload);
-
-      if (item && item.quantity > 1) {
-        item.quantity -= 1;
-      }
-
-      break;
-    }
-    default:
-      return draft;
-  }
-}, []);
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [cartItems, dispatch] = useReducer(cartReducer, []);
--- src/app/store/favorites-context.tsx
+++ src/app/store/favorites-context.tsx
@@ -1,35 +1,13 @@
-import { produce } from "immer";
 import { createContext, ReactNode, useContext, useReducer } from "react";
 import {
   FAVORITES_ACTION_TYPES,
   IFavoriteItem,
-  IFavoritesAction,
   IFavoritesContextType,
 } from "../types";
+import { favoritesReducer } from "./reducers";
 
 const FavoritesContext = createContext<IFavoritesContextType | undefined>(
   undefined
-);
-
-const favoritesReducer = produce(
-  (draft: IFavoriteItem[], action: IFavoritesAction) => {
-    switch (action.type) {
-      case FAVORITES_ACTION_TYPES.TOGGLE_FAVORITES: {
-        const index = draft.findIndex((item) => item.id === action.payload.id);
-
-        if (index !== -1) {
-          draft.splice(index, 1);
-        } else {
-          draft.push(action.payload);
-        }
-
-        break;
-      }
-      default:
-        return draft;
-    }
-  },
-  []
 );
 
 export const FavoritesProvider = ({ children }: { children: ReactNode }) => {
